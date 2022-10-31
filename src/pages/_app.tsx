b7605764680f2acb60bfba308974414c013d9ff5
Robert-Jan van der Elst <rjvdelst@gmail.com>
2022-10-31 20:37:27 +0100

    wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
+import { Analytics } from '@vercel/analytics/react'
 import type { AppProps } from 'next/app'
 import '../../tailwind.css'
 import Base from '../components/layout/Base'
 
 export default function Blog({ Component, pageProps }: AppProps) {
   return (
-    <Base>
-      <Component {...pageProps} />
-    </Base>
+    <>
+      <Base>
+        <Component {...pageProps} />
+      </Base>
+      <Analytics />
+    </>
   )
 }
