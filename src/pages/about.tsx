9e5bd8a7d8ca5b1645145ef6bcfee17198190944
Robert-Jan <5645326+Robert-Jan@users.noreply.github.com>
2023-05-20 17:00:26 +0200

    Update

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -54,7 +54,7 @@ export default function About(): JSX.Element {
             />
           </div>
 
-          <div className="rounded-2xl border border-zinc-100 mt-6 p-6 dark:border-zinc-700/40">
+          <div className="mt-6 rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
             <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
               <svg
                 viewBox="0 0 24 24"
@@ -64,7 +64,12 @@ export default function About(): JSX.Element {
                 stroke-linejoin="round"
                 aria-hidden="true"
                 className="h-6 w-6 flex-none">
-                 <path stroke-linecap="round" stroke-linejoin="round" className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
+                  d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"
+                />
               </svg>
 
               <span className="ml-3 mt-0.5">Certifications</span>
@@ -89,8 +94,7 @@ export default function About(): JSX.Element {
                     Microsoft Azure Fundamentals
                   </dd>
                   <dd className="text-xs text-zinc-500 dark:text-zinc-400">AZ-900</dd>
-                  <dd
-                    className="ml-auto text-xs text-zinc-400 dark:text-zinc-500">
+                  <dd className="ml-auto text-xs text-zinc-400 dark:text-zinc-500">
                     <time dateTime="2023">2023</time>
                   </dd>
                 </dl>
@@ -114,10 +118,8 @@ export default function About(): JSX.Element {
                     Azure IoT Developer Specialty
                   </dd>
                   <dd className="text-xs text-zinc-500 dark:text-zinc-400">AZ-220</dd>
-                  <dd
-                    className="ml-auto text-xs text-zinc-400 dark:text-zinc-500">
-                    <time dateTime="2023">2023</time>
-                    {" "}<span aria-hidden="true">—</span>{" "}
+                  <dd className="ml-auto text-xs text-zinc-400 dark:text-zinc-500">
+                    <time dateTime="2023">2023</time> <span aria-hidden="true">—</span>{" "}
                     <time dateTime="2025">2025</time>
                   </dd>
                 </dl>
